58f4195e6addef19b074f8cd61f13f0080fe7553
Rogério <roger.lista@gmail.com>
2020-05-06 13:00:43 -0300

    Estudo JS - funções que não encapsulam condicionais

---
+++ functions/nao-encapsular-condicionais.js
@@ -0,0 +1,11 @@
+// Evite
+if (person.age >= 18 && person.age <= 30) {
+  //...
+}
+
+// Prefira
+const isACoolPerson = (person) => person.age >= 18 && person.age <= 30
+
+if (isACoolPerson(person)) {
+  //...
+}
