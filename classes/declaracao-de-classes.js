c452385dd5643ef5e633de12cf133487eb384320
Rogério <roger.lista@gmail.com>
2020-05-14 13:28:02 -0300

    Estudo JS - formas de declarar uma classe

---
+++ classes/declaracao-de-classes.js
@@ -0,0 +1,20 @@
+class MyClass {
+  constructor() {}
+
+  method01() {}
+  method02() {}
+}
+
+// Outras formas de declarar uma classe.
+
+const myConst = class MyClass {}
+
+// ou
+
+const myConst = class {}
+
+// ou
+
+function MakeClass() {
+  return class {}
+}
